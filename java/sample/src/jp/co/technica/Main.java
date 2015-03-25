90016e08feb15eb41cbadce64f715b2493aee77e
taisuke-uchida <taisuke_uchida@technical-g.co.jp>
2015-03-26 00:26:50 +0900

    sampleプロジェクト

---
+++ java/sample/src/jp/co/technica/Main.java
@@ -0,0 +1,11 @@
+package jp.co.technica;
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World !!");
+    }
+}
